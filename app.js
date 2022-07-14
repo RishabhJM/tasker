f0c17b87aad801384d00c6472cf2f8f16ef39d5f
Shyamal <08.shyamal@gmail.com>
2022-07-14 15:29:56 +0530

    yoo

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect(process.env.DB_LINK);
+mongoose.connect("mongodb+srv://Tasker:31ifeT78acS16Try@cluster0.f7ueu.mongodb.net/?retryWrites=true&w=majority");
 
 const userSchema = new mongoose.Schema({
   name: String,
