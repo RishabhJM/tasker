28459f52ecd79d0ee4f041813cfa2354aee282bc
Rishabh <www.majjurocks@gmail.com>
2022-07-14 15:19:26 +0530

    commie

--- .history/app_20220714151710.js
+++ .history/app_20220714151710.js
@@ -69,7 +69,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect(String(`${process.env.DB_LINK}`));
+mongoose.connect(String(`{process.env.DB_LINK}));
 
 const userSchema = new mongoose.Schema({
   name: String,
